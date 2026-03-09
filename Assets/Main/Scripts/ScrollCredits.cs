76933cce6751f63cda1b40b8a09e168bc4e94940
Jarb900 <jarb900@hotmail.com>
2026-03-09 16:41:22 +0000

    stuff

--- Assets/Main/Scripts/ScrollCredits.cs
+++ Assets/Main/Scripts/ScrollCredits.cs
@@ -4,11 +4,20 @@ namespace Main.Scripts
 {
     public class ScrollCredits : MonoBehaviour
     {
-        public float scrollSpeed = 10f;
+        public float scrollSpeed = 50f; // You may need a higher number since we are moving in UI pixels now
+        
+        private RectTransform rectTransform;
+
+        void Start()
+        {
+            // Grab the RectTransform component when the script starts
+            rectTransform = GetComponent<RectTransform>();
+        }
 
         void Update()
         {
-            transform.Translate(Vector3.up * scrollSpeed * Time.deltaTime);
+            // Move the Y anchored position up over time
+            rectTransform.anchoredPosition += Vector2.up * scrollSpeed * Time.deltaTime;
         }
     }
 }
